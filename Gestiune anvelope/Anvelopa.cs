0c70bf38a5b5ff5f46cff6e647159c91c80461a9
Alex <Alex@DESKTOP-GG2HRLJ>
2023-03-15 13:30:58 +0200

    Add project files.

---
+++ Gestiune anvelope/Anvelopa.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestiune_anvelope
+{
+    class Anvelopa
+    {
+        string producator;
+        string tip;
+        int latime;
+        int inaltime;
+        string raza;
+        int nrBucati;
+        float pret;
+        public Anvelopa(string producator_,string tip_ ,int latime_,int inaltime_,string raza_,int nrBucati_,float pret_)
+        {
+             producator=producator_;
+             tip=tip_;
+             latime=latime_;
+             inaltime=inaltime_;
+             raza=raza_;
+             nrBucati=nrBucati_;
+             pret=pret_;
+        }
+
+        public string Producator
+        {
+            get { return producator; }
+            set { producator = value; }
+        }
+        public string Tip
+        {
+            get { return tip; }
+            set { tip = value; }
+        }
+        public int Latime
+        {
+            get { return latime; }
+            set { latime = value; }
+        }
+        public int Inaltime
+        {
+            get { return inaltime; }
+            set { inaltime = value; }
+        }
+        public string Raza
+        {
+            get { return raza; }
+            set { raza = value; }
+        }
+        public int NrBucati
+        {
+            get { return nrBucati; }
+            set { nrBucati = value; }
+        }
+        public float Pret
+        {
+            get { return pret; }
+            set { pret = value; }
+        }
+        public override string ToString()
+        {
+            return "Producator: " + producator + ", Tip: "+tip+", Latime: "+latime+", Inaltime: "+inaltime+", raza: "+raza+", Numar Bucati: "+nrBucati+", pret: "+pret;
+        }
+        public  string ToFile()
+        {
+            return  producator + "," + tip + "," + latime + "," + inaltime + "," + raza + "," + nrBucati + "," + pret;
+        }
+
+    }
+}
---
+++ Gestiune anvelope/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestiune_anvelope
+{
+    class Program
+    {
+       public static List<Anvelopa> toateAnvelopele = new List<Anvelopa>();
+        static void Main()
+        {
+           
+            //initializare lista cu taote anvelopele din fisier
+            Readfromfile(toateAnvelopele);
+            Boolean showMenu_ = true;
+            while(showMenu_)
+            {
+                showMenu_ = showMenu();
+            }
+            
+            // Writetofile();
+        }
+
+        public static Boolean showMenu()
+        {
+            Console.WriteLine("--==MENIU GESTIUNE DEPOZIT ANVELOPE==--");
+            Console.WriteLine("1. Afiseaza taote anvelopele");
+            Console.WriteLine("2. Adauga anvelopa");
+            Console.WriteLine("3. Modifica anvelopa");
+            Console.WriteLine("4. Sterge anvelopa");
+            Console.WriteLine("Alegeti o optiune din meniu: ");
+            
+            
+                switch (Console.ReadLine())
+                {
+                    case "1":
+
+                        foreach (Anvelopa anv in toateAnvelopele)
+                        {
+                            //afiseaza taote anvelopele din fisier dupa ce au fost convertite in obiecte
+                            Console.WriteLine(anv.ToString());
+                        }
+                        return  true;
+                        
+                    case "2":
+                    Console.WriteLine("Introduceti producatorul");
+                    string producator=Console.ReadLine();
+                    Console.WriteLine("Introduceti tipul anvelopei (iarna/vara)");
+                    string tip = Console.ReadLine();
+                    Console.WriteLine("Introduceti latime");
+                    int latime = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Introduceti inaltime");
+                    int inaltime = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Introduceti raza ( ex :R16,R18)");
+                    string raza = Console.ReadLine();
+                    Console.WriteLine("Introduceti nr bucati");
+                    int nrBucati = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Introduceti pret");
+                    float pret = float.Parse(Console.ReadLine());
+
+                    Anvelopa anvelopaNoua = new Anvelopa(producator, tip, latime, inaltime, raza, nrBucati, pret);
+                    toateAnvelopele.Add(anvelopaNoua);
+                    Writetofile();
+
+                    return true;
+                case "3":
+                    return true;
+                case "4":
+                    return true;
+                default:
+                        Console.WriteLine("Optiune incorecta!!!Alegeti o optiune din meniu: ");
+                    return true;
+            }
+            
+        }
+        static void adaugaAnvelopa(Anvelopa anvelopaDeAdaugat)
+        {
+
+        }
+        static void Readfromfile(List<Anvelopa> toateAnvelopele)
+        {
+            string line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Users\\Axellu\\source\\repos\\Gestiune anvelope\\gestiune.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    
+                  //convert line from file to objerct Anvelopa
+                  string[] result=  line.Split(',');
+                    toateAnvelopele.Add(new Anvelopa(result[0], result[1],int.Parse( result[2]),int.Parse( result[3]), result[4],int.Parse( result[5]),float.Parse( result[6])));
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+        static void Writetofile()
+        {
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw =new StreamWriter("C:\\Users\\Axellu\\source\\repos\\Gestiune anvelope\\gestiune.txt");
+                //Write a line of text
+                foreach (Anvelopa anv in toateAnvelopele)
+                {
+                    sw.WriteLine(anv.ToFile());
+                }
+                
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+        }
+}
